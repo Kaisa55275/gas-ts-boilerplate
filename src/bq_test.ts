b59942a662d757fb30b9ee451783186b25c4ecc7
DAIKI NIMURA <nimuradaiki0730@gmail.com>
2020-09-11 15:30:51 +0900

    deleted bq test

--- src/bq_test.ts
+++ src/bq_test.ts
@@ -1,21 +1,23 @@
+declare var BigQuery: GoogleAppsScript.Bigquery;
+
 const function1 = (): void => {
   const projectId = 'smooth-prod'
-  const ss = SpreadsheetApp.openById('1sfbnz8iP2KhrhAbfD8gPksrbFmsvmvgCbdB2_ITH_fk')
-  const targetSheet = ss.getSheetByName('バージョン毎の日程登録率')
+  const ss = SpreadsheetApp.openById('*** スプレッドシートのID ***')
+  const targetSheet = ss.getSheetByName('***シート名***')
 
   const runQuery = (): GoogleAppsScript.Bigquery.Schema.QueryResponse => {
+    const query = `
+      SELECT count(*) FROM \`***テーブル名***\`
+      WHERE
+        ***クエリ***
+    `
     const request: GoogleAppsScript.Bigquery.Schema.QueryRequest = {
-      query: `
-        SELECT count(*) FROM 'smooth-prod.analyse_table.format_mysql_dump'
-        WHERE
-          partition_date = "2020-09-11"
-          and user_is_test is not true
-          and user_id >= 1944
-          and ses_result = "通過"
-          and not((search_status & 2) > 0)
-      `
+      query,  
+      useLegacySql: false
     }
-    const result = Bigquery.Jobs?.query(request, projectId)
+
+
+    const result = BigQuery.Jobs?.query(request, projectId)
 
     if (!result) {
       throw new Error('no result')
